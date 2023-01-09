40da2aecc0e1ea7b311243515f89dc48c49d8f8f
rzceoffical <95528599+rzceoffical@users.noreply.github.com>
2023-01-09 12:59:09 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className='App'>
+      <img className='logo' src='../public/vite.svg' alt='' />
+      <h1>ReactJS + Vite + TS</h1>
+    </div>
+  )
+}
+
+export default App
